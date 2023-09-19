95f54500bbd358bb14c062b07ae1f6b8f4df7e60
fjl <fanjl22@mails.tsinghua.edu.cn>
2023-09-19 18:51:33 +0800

    correct some style error

--- src/EdcHost.Tests/UnitTests/Games/EventArgs/PlayerMoveEventArgsTest.cs
+++ src/EdcHost.Tests/UnitTests/Games/EventArgs/PlayerMoveEventArgsTest.cs
@@ -1,23 +1,18 @@
+using EdcHost.Games;
 using Xunit;
 using Moq;
-using EdcHost.Games;
-namespace  EdcHost.Tests.UnitTests.Games.EventArgs;
+
+namespace EdcHost.Tests.UnitTests.Games.EventArgs;
 
 public class PlayerMoveEventArgsTests
 {
     [Fact]
     public void PlayerMoveEventArgs_CorrectlyInitialized()
     {
-        // Arrange
         var playerMock = new Mock<IPlayer>();
         var positionBeforeMovementMock = new Mock<IPosition<float>>();
         var positionMock = new Mock<IPosition<float>>();
-
-        // Act
-        var args = new PlayerMoveEventArgs(playerMock.Object, 
-            positionBeforeMovementMock.Object, positionMock.Object);
-
-        // Assert
+        var args = new PlayerMoveEventArgs(playerMock.Object, positionBeforeMovementMock.Object, positionMock.Object);
         Assert.NotNull(args);
         Assert.Equal(playerMock.Object, args.Player);
         Assert.Equal(positionBeforeMovementMock.Object, args.PositionBeforeMovement);
