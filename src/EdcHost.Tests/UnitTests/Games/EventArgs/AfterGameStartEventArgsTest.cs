66490db5b0240c861b6e0af6987f16747c4e6b7c
fjl <fanjl22@mails.tsinghua.edu.cn>
2023-09-24 11:39:49 +0800

    fix problems in AfterGameStartEventArgs.cs

--- src/EdcHost.Tests/UnitTests/Games/EventArgs/AfterGameStartEventArgsTest.cs
+++ src/EdcHost.Tests/UnitTests/Games/EventArgs/AfterGameStartEventArgsTest.cs
@@ -9,11 +9,11 @@ public class AfterGameStartEventArgsTest
     [Fact]
     public void AfterGameStartEventArgs_CorrectlyInitialized()
     {
-        var playerMock = new Mock<IPlayer>();
+        var gameMock = new Mock<IGame>();
         DateTime? dateTime = new DateTime(2023, 10, 1, 0, 0, 0);
-        var args = new AfterGameStartEventArgs(playerMock.Object, dateTime);
+        var args = new AfterGameStartEventArgs(gameMock.Object, dateTime);
         Assert.NotNull(args);
-        Assert.Equal(playerMock.Object, args.Player);
-        Assert.Equal(dateTime, args.DateTime);
+        Assert.Equal(gameMock.Object, args.Game);
+        Assert.Equal(dateTime, args.StartTime);
     }
 }
