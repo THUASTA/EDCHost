a412b1b65f882eae4dcc8b3e4692a9df1a02fe18
Sayur1n <143185211+Sayur1n@users.noreply.github.com>
2023-10-17 18:45:57 +0800

    Update PacketFromSlave.cs
    写了与小车间包的生成和解包过程

--- src/EdcHost/SlaveServers/PacketFromSlave.cs
+++ src/EdcHost/SlaveServers/PacketFromSlave.cs
@@ -1,19 +1,26 @@
+using System.Data.SqlTypes;
+
 namespace EdcHost.SlaveServers;
 
 public class PacketFromSlave : IPacketFromSlave
 {
     const int PACKET_LENGTH = 10;
-    public int ActionType { get; }
-    public int Param { get; }
+    public int ActionType { get;private set; }
+    public int Param { get;private set; }               
 
     public byte[] MakePacket()
     {
         byte[] bytes = new byte[PACKET_LENGTH];
         // TODO: add a serializer
+        bytes[0]=Convert.ToByte(ActionType);
+        bytes[1]=Convert.ToByte(Param);
+
         return bytes;
     }
     public void ExtractPacketData(byte[] bytes)
     {
         //TODO: add a deserializer
+        ActionType=Convert.ToInt32(bytes[0]);
+        Param=Convert.ToInt32(bytes[1]);
     }
 }
