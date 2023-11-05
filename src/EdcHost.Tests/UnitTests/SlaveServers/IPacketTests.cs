efb9e23652da46e9dbbe4421903e6c98d8719bbc
fjl <fanjl22@mails.tsinghua.edu.cn>
2023-11-05 21:21:01 +0800

    Add Tests for PacketFromSlave and IPacket

---
+++ src/EdcHost.Tests/UnitTests/SlaveServers/IPacketTests.cs
@@ -0,0 +1,24 @@
+using Xunit;
+
+namespace EdcHost.SlaveServers;
+public class IPacketTests
+{
+    [Fact]
+    public void CalculateChecksum_ReturnCorrectValue()
+    {
+        byte[] bytes = new byte[] { 0x01, 0x02, 0x03, 0x04 };
+        byte checksum = IPacket.CalculateChecksum(bytes);  
+        Assert.Equal(0x0F, checksum);
+    }
+
+    [Fact]
+    public void GetPacketData_ReturnCorrectValue()
+    {
+        byte[] data = new byte[] { 0x01, 0x02, 0x03, 0x04 };  
+        byte[] header = IPacket.GeneratePacketHeader(data);  
+        Assert.Equal(0x55, header[0]);  
+        Assert.Equal(0xAA, header[1]);  
+        Assert.Equal(data.Length, BitConverter.ToInt16(header, 2)); 
+    }
+
+}
--- src/EdcHost.Tests/UnitTests/SlaveServers/PacketFromHostTests.cs
+++ src/EdcHost.Tests/UnitTests/SlaveServers/PacketFromHostTests.cs
@@ -1,17 +1,76 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EdcHost.Games;
 using Xunit;
 
-namespace EdcHost.Tests.UnitTests.SlaveServers
+namespace EdcHost.SlaveServers;
+
+public class PacketFromHostTests
 {
-    public class SlaveserverFromPocket
+    [Fact]
+    public void PacketFromHost_CorrectlyInitialized()
     {
-        [Fact]
-        public void Test1()
-        {
-            Assert.True(true);
-        }
+        int exp_gameStage = 0;
+        int exp_elapsedTime = 12000;
+        List<int> exp_heightOfChunks = new List<int>{1, 2, 3, 6, 7};
+        bool exp_hasBed = true;
+        bool exp_hasBedOpponent = true;
+        float exp_positionX = 0.4f;
+        float exp_positionY = 0.4f;
+        float exp_positionOpponentX = 7.4f;
+        float exp_positionOpponentY = 7.4f;
+        int exp_agility =  0;
+        int exp_health = 8;
+        int exp_MaxHealth = 8;
+        int exp_strength = 4;
+        int exp_emeraldCount = 64;
+        int exp_woolcount = 64;
+        PacketFromHost packetFromHost = new PacketFromHost(
+            exp_gameStage, exp_elapsedTime, exp_heightOfChunks, exp_hasBed, exp_hasBedOpponent,
+            exp_positionX, exp_positionY, exp_positionOpponentX, exp_positionOpponentY, exp_agility, 
+            exp_health, exp_MaxHealth, exp_strength, exp_emeraldCount, exp_woolcount);
+        Assert.Equal(exp_gameStage, packetFromHost.GameStage);
+        Assert.Equal(exp_elapsedTime, packetFromHost.ElapsedTime);
+        Assert.Equal(exp_heightOfChunks, packetFromHost.HeightOfChunks);
+        Assert.Equal(exp_hasBed, packetFromHost.HasBed);
+        Assert.Equal(exp_hasBedOpponent, packetFromHost.HasBedOpponent);
+        Assert.Equal(exp_positionX, packetFromHost.PositionX);
+        Assert.Equal(exp_positionY, packetFromHost.PositionY);
+        Assert.Equal(exp_positionOpponentX, packetFromHost.PositionOpponentX);
+        Assert.Equal(exp_positionY, packetFromHost.PositionOpponentY);
+        Assert.Equal(exp_agility, packetFromHost.Agility);
+        Assert.Equal(exp_health, packetFromHost.Health);
+        Assert.Equal(exp_MaxHealth, packetFromHost.MaxHealth);
+        Assert.Equal(exp_strength, packetFromHost.Strength);
+        Assert.Equal(exp_strength, packetFromHost.Strength);
+        Assert.Equal(exp_emeraldCount, packetFromHost.EmeraldCount);
+        Assert.Equal(exp_woolcount, packetFromHost.WoolCount);
     }
+
+    [Fact]
+    public void ToBytes_CorrectlyCoverted()
+    {
+        int exp_gameStage = 0;
+        int exp_elapsedTime = 12000;
+        List<int> exp_heightOfChunks = new List<int>{1, 2, 3, 6, 7};
+        bool exp_hasBed = true;
+        bool exp_hasBedOpponent = true;
+        float exp_positionX = 0.4f;
+        float exp_positionY = 0.4f;
+        float exp_positionOpponentX = 7.4f;
+        float exp_positionOpponentY = 7.4f;
+        int exp_agility =  0;
+        int exp_health = 8;
+        int exp_MaxHealth = 8;
+        int exp_strength = 4;
+        int exp_emeraldCount = 64;
+        int exp_woolcount = 64;
+        PacketFromHost exp_Packet = new PacketFromHost(
+            exp_gameStage, exp_elapsedTime, exp_heightOfChunks, exp_hasBed, exp_hasBedOpponent,
+            exp_positionX, exp_positionY, exp_positionOpponentX, exp_positionOpponentY, exp_agility, 
+            exp_health, exp_MaxHealth, exp_strength, exp_emeraldCount, exp_woolcount);
+        byte[] data = new byte[39];
+        data = exp_Packet.ToBytes();
+        PacketFromHost actualPacket = new PacketFromHost(data);
+        Assert.Equal(exp_Packet, actualPacket);
+    }
+    
 }
